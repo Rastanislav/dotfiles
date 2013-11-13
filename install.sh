18a61b7cabb81745bac0cbf081ce7f3d6ed5639e
stanislavs <stanislavs@LVALTP0558.(none)>
2013-11-13 18:24:53 +0200

    created dotfiles dir

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+
+# Clone my oh-my-zsh repository from GitHub only if it isn't already present
+if [[ ! -d $dir/dotfiles/ ]]; then
+    git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+fi
